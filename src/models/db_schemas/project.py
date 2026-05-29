8873db3cfb82e80a742afffd2017fbf7af16b717
AhmadMajde22 <ahmadbara58.ad@gmail.com>
2026-05-29 10:30:11 +0300

    setup the Schemas and Model for Mongo DB

--- src/models/db_schemas/project.py
+++ src/models/db_schemas/project.py
@@ -4,17 +4,14 @@ from bson.objectid import ObjectId
 
 
 class Project(BaseModel):
-    _id: Optional[ObjectId]
-    project_id: str = Field(...,min_length=1)
-
-
+    id: Optional[ObjectId] = Field(None, alias="_id")
+    project_id: str = Field(..., min_length=1)
 
     @field_validator("project_id")
-    def validate_project_id(cls,value):
+    def validate_project_id(cls, value):
         if not value.isalnum():
             raise ValueError("prooject_id must be alphanumeric")
         return value
-
 
     class Config:
         arbitrary_types_allowed = True
