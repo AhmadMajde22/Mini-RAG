ad56b56cb086d6b5bef4d78868b62bf162cb22bf
AhmadMajde22 <ahmadbara58.ad@gmail.com>
2026-06-06 23:44:14 +0300

    Setup DB Indexing

--- src/routes/data.py
+++ src/routes/data.py
@@ -25,7 +25,7 @@ async def upload_data(
     app_settings: Settings = Depends(get_settings),
 ):
 
-    project_model = ProjectModel(db_client=request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
 
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
@@ -75,9 +75,9 @@ async def process_file(
     overlap_size = process_request.overlap_size
     do_reset = process_request.do_reset
 
-    project_model = ProjectModel(db_client=request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
 
-    chunk_model = ChunkModel(db_client=request.app.db_client)
+    chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
 
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
