e65692882d306bd7ce91470fbbb2f7de49049129
AhmadMajde22 <ahmadbara58.ad@gmail.com>
2026-07-04 21:56:45 +0300

    setup SQLalchemy models and alembic migrations

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 from helpers.config import get_settings
 from routes import base, data, nlp
@@ -13,8 +14,18 @@ from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 async def startup_span(app: FastAPI):
     settings = get_settings()
 
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    postgres_conn = f"postgreql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+
+    app.db_engine = create_async_engine(postgres_conn)
+
+
+
+    app.db_client =sessionmaker(
+        app.db_engine,
+        class_ = AsyncSession,
+        expire_on_commit=False,
+
+    )
 
     llm_provider_factory = LLMProviderFactory(settings)
     vectordb_provider_factory = VectorDBProviderFactory(settings)
@@ -39,7 +50,7 @@ async def startup_span(app: FastAPI):
 
 
 async def shutdown_span(app: FastAPI):
-    app.mongo_conn.close()
+    app.db_engine.dispose()
     app.vectordb_client.disconnect()
 
 
